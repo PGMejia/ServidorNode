fa9dbfffe5d3b9c2936864ce93ecd0aeede5b88f
Pablo <PGMejia@hotmail.com>
2023-06-12 09:45:38 -0500

    Cambios

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
+const libros=[
+    {'titulo':'El señor de los anillos', 'autor':'J.R.R. Tolkien'},
+    {'titulo':'El hobbit', 'autor':'J.R.R. Tolkien'},
+    {'titulo':'Cancion de hielo y fuego', 'autor':'George R.R. Martin'},
+    {'titulo':'Choque de reyes', 'autor':'George R.R. Martin'},
+    {'titulo':'Tormenta de espadas', 'autor':'George R.R. Martin'},
+    {'titulo':'Festin de cuervos', 'autor':'George R.R. Martin'},
+    {'titulo':'Danza de dragones', 'autor':'George R.R. Martin'},
+    {'titulo':'Juego de tronos', 'autor':'George R.R. Martin'},
+    {'titulo':'El nombre del viento', 'autor':'Patrick Rothfuss'},
+    {'titulo':'El temor de un hombre sabio', 'autor':'Patrick Rothfuss'}
+    
+];
+
 const server = http.createServer((req, res) => {
-    const {headers, url, method} = req;
-    console.log(headers, url, method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello World</h1>');
     res.end();
 });
 
